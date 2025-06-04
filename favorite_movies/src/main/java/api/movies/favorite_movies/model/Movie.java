03e82c1846be98dc78cbbb70195861d552d2ed9f
Vasile <vasile.ceban1@isa.utm.md>
2025-06-04 11:13:43 +0300

    movie entity

---
+++ favorite_movies/src/main/java/api/movies/favorite_movies/model/Movie.java
@@ -0,0 +1,28 @@
+package api.movies.favorite_movies.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "movies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String title;
+    
+    private String director;
+    private Integer year;
+    private String genre;
+    private String posterUrl;
+    private String description;
+    private Double rating;
+}
