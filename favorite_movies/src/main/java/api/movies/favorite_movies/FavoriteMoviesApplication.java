c9e21a55cb04d9a2a6b1257311ba05854166a23d
Vasile <vasile.ceban1@isa.utm.md>
2025-06-04 11:10:44 +0300

    enable CORS

--- favorite_movies/src/main/java/api/movies/favorite_movies/FavoriteMoviesApplication.java
+++ favorite_movies/src/main/java/api/movies/favorite_movies/FavoriteMoviesApplication.java
@@ -2,12 +2,32 @@ package api.movies.favorite_movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableWebMvc
+@OpenAPIDefinition(info = @Info(title = "Favorite Movies API", version = "1.0", description = "CRUD API for favorite movies application"))
 public class FavoriteMoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FavoriteMoviesApplication.class, args);
 	}
-
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:3000"); // React app URL
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
