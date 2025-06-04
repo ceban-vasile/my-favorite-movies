ccb72bd26e363e368c3fa379b1c927ba8f4f0918
Vasile <vasile.ceban1@isa.utm.md>
2025-06-04 11:12:50 +0300

    create the Role enum and Permission enum to define user roles and their permissions

---
+++ favorite_movies/src/main/java/api/movies/favorite_movies/model/Permission.java
@@ -0,0 +1,7 @@
+package api.movies.favorite_movies.model;
+
+public enum Permission {
+    READ,
+    WRITE,
+    DELETE
+}
---
+++ favorite_movies/src/main/java/api/movies/favorite_movies/model/Role.java
@@ -0,0 +1,7 @@
+package api.movies.favorite_movies.model;
+
+public enum Role {
+    ADMIN,
+    WRITER,
+    VISITOR
+}
