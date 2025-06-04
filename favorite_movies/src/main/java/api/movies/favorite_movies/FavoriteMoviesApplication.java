e4bf5a435a61f9d48fcd2763e60f4c8586ff948b
Vasile <vasile.ceban1@isa.utm.md>
2025-06-04 11:00:58 +0300

    create a spring boot app for restAPI and JWT tokens

---
+++ favorite_movies/src/main/java/api/movies/favorite_movies/FavoriteMoviesApplication.java
@@ -0,0 +1,13 @@
+package api.movies.favorite_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteMoviesApplication.class, args);
+	}
+
+}
---
+++ favorite_movies/src/test/java/api/movies/favorite_movies/FavoriteMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.movies.favorite_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
